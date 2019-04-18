7610c3d661f0dedc48989194393b0d06dd9cf111
M.Charattrakool <m.charattrakool@gmail.com>
2019-04-18 09:57:14 +0700

    final version

---
+++ script_treemap_import.js
@@ -0,0 +1,225 @@
+let domImport = document.getElementById("treemap_import");
+let importChart = echarts.init(domImport);
+let importApp = {};
+option = null;
+importChart.showLoading();
+
+$.get(
+  "https://monthol8th.github.io/01204314-assignment2/data/egy-import.json",
+  function(egyptData) {
+    $.get(
+      "https://monthol8th.github.io/01204314-assignment2/data/zaf-import.json",
+      function(zafData) {
+        $.get(
+          "https://monthol8th.github.io/01204314-assignment2/data/mar-import.json",
+          function(marData) {
+            $.get(
+              "https://monthol8th.github.io/01204314-assignment2/data/ken-import.json",
+              function(kenData) {
+                $.get(
+                  "https://monthol8th.github.io/01204314-assignment2/data/cmr-import.json",
+                  function(cmrData) {
+                    console.log("import");
+                    importChart.hideLoading();
+
+                    function withPostfix(labelValue) {
+                      // Nine Zeroes for Billions
+                      return Math.round(labelValue) >= 1.0e9
+                        ? (labelValue / 1.0e9).toFixed(2) + " Billion"
+                        : // Six Zeroes for Millions
+                        Math.round(labelValue) >= 1.0e6
+                        ? (labelValue / 1.0e6).toFixed(2) + " Million"
+                        : // Three Zeroes for Thousands
+                        labelValue >= 1.0e3
+                        ? (labelValue / 1.0e3).toFixed(2) + " Thousand"
+                        : labelValue.toFixed(2);
+                    }
+
+                    function colorMimportAppingChange(value) {
+                      let leveloption = getLeveloption(value);
+                      chart.setOption({
+                        series: [
+                          {
+                            levels: leveloption
+                          }
+                        ]
+                      });
+                    }
+
+                    let formatUtil = echarts.format;
+
+                    function getLeveloption() {
+                      return [
+                        {
+                          itemStyle: {
+                            normal: {
+                              borderColor: "#777",
+                              borderWidth: 0,
+                              gapWidth: 1
+                            }
+                          },
+                          upperLabel: {
+                            normal: {
+                              show: false
+                            }
+                          }
+                        },
+                        {
+                          itemStyle: {
+                            normal: {
+                              borderColor: "#555",
+                              borderWidth: 5,
+                              gapWidth: 1
+                            },
+                            emphasis: {
+                              borderColor: "#ddd"
+                            }
+                          }
+                        },
+                        {
+                          colorSaturation: [0.35, 0.5],
+                          itemStyle: {
+                            normal: {
+                              borderWidth: 5,
+                              gapWidth: 1,
+                              borderColorSaturation: 0.6
+                            }
+                          }
+                        }
+                      ];
+                    }
+
+                    let modes = [
+                      "Egypt",
+                      "South Africa",
+                      "Kenya",
+                      "Cameroon",
+                      "Morocco"
+                    ];
+                    let data = [egyptData, zafData, kenData, cmrData, marData];
+                    importChart.setOption(
+                      (option = {
+                        tooltip: {
+                          formatter: function(info) {
+                            let name = info.name.split(";").join("<br>&#183; ");
+                            let price = formatUtil.addCommas(info.value[0]);
+                            let percent = info.value[1].toFixed(2);
+                            let sectionpercent = info.value[2].toFixed(2);
+
+                            return [
+                              '<div class="tooltip-title">' + name + "</div>",
+                              "$" + price,
+                              percent + "% total",
+                              sectionpercent + "% section"
+                            ].join("<br>");
+                          }
+                        },
+
+                        legend: {
+                          data: modes,
+                          selectedMode: "single",
+                          itemGap: 5,
+                          backgroundColor: "rgb(243,243,243)",
+                          borderRadius: 5
+                        },
+
+                        series: modes.map(function(mode, idx) {
+                          let seriesOpt = {
+                            name: mode,
+                            type: "treemap",
+                            visibleMin: 300,
+                            label: {
+                              normal: {
+                                formatter: function(params) {
+                                  let arr = [
+                                    "{name|" + params.name + "}",
+                                    "{hr|}",
+                                    "{budget|$ " +
+                                      withPostfix(params.value[0]) +
+                                      "} {label|USD}",
+                                    "{household| " +
+                                      params.value[1].toFixed(2) +
+                                      "% total}",
+                                    "{household| " +
+                                      echarts.format.addCommas(
+                                        params.value[2].toFixed(2)
+                                      ) +
+                                      "% of parent section}"
+                                  ];
+                                  return arr.join("\n");
+                                },
+                                rich: {
+                                  budget: {
+                                    fontSize: 22,
+                                    lineHeight: 30,
+                                    color: "yellow"
+                                  },
+                                  household: {
+                                    fontSize: 14,
+                                    color: "#fff"
+                                  },
+                                  label: {
+                                    fontSize: 9,
+                                    backgroundColor: "rgba(0,0,0,0.3)",
+                                    color: "#fff",
+                                    borderRadius: 2,
+                                    padding: [2, 4],
+                                    lineHeight: 25,
+                                    align: "right"
+                                  },
+                                  name: {
+                                    fontSize: 12,
+                                    color: "#fff"
+                                  },
+                                  hr: {
+                                    width: "100%",
+                                    borderColor: "rgba(255,255,255,0.2)",
+                                    borderWidth: 0.5,
+                                    height: 0,
+                                    lineHeight: 10
+                                  }
+                                }
+                              }
+                            },
+                            leafDepth: 1,
+                            upperLabel: {
+                              normal: {
+                                show: true,
+                                formatter: function(params) {
+                                  let arr = [
+                                    params.name,
+                                    "($ " + withPostfix(params.value[0]) + ")"
+                                  ];
+                                  return arr.join(" ");
+                                },
+                                height: 45
+                              }
+                            },
+                            itemStyle: {
+                              normal: {
+                                borderColor: "#555",
+                                gapWidth: 1,
+                                borderwidth: 3
+                              }
+                            },
+                            levels: getLeveloption(),
+                            data: data[idx]
+                          };
+                          return seriesOpt;
+                        })
+                      })
+                    );
+                    console.log("imported");
+                  }
+                );
+              }
+            );
+          }
+        );
+      }
+    );
+  }
+);
+if (option && typeof option === "object") {
+  importChart.setOption(option, true);
+}
